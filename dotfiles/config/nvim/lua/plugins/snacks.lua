4747ba7b747b97e62aeabd7b17f7851bfa5ed144
Vardan Pogosian <vardan.pogosyan@gmail.com>
2025-08-06 21:46:04 +0500

    dotfiles: nvim: reformatted folke/snacks.nvim config

--- dotfiles/config/nvim/lua/plugins/snacks.lua
+++ dotfiles/config/nvim/lua/plugins/snacks.lua
@@ -1,30 +1,30 @@
 return {
-    {
-        "folke/snacks.nvim",
-        opts = {
-            picker = {
-                layout = {
-                    preset = "dropdown",
-                    layout = {
-                        backdrop = false,
-                        width = 0.9,
-                        min_width = 120,
-                        height = 0.9,
-                        border = "none",
-                        box = "vertical",
-                        {
-                            box = "vertical",
-                            border = "rounded",
-                            title = "{title} {live} {flags}",
-                            title_pos = "center",
-                            height = 0.3,
-                            { win = "input", height = 1,     border = "bottom" },
-                            { win = "list",  border = "none" },
-                        },
-                        { win = "preview", title = "{preview}", height = 0, border = "rounded" },
-                    },
-                },
+  {
+    "folke/snacks.nvim",
+    opts = {
+      picker = {
+        layout = {
+          preset = "dropdown",
+          layout = {
+            backdrop = false,
+            width = 0.9,
+            min_width = 120,
+            height = 0.9,
+            border = "none",
+            box = "vertical",
+            {
+              box = "vertical",
+              border = "rounded",
+              title = "{title} {live} {flags}",
+              title_pos = "center",
+              height = 0.3,
+              { win = "input", height = 1, border = "bottom" },
+              { win = "list", border = "none" },
             },
+            { win = "preview", title = "{preview}", height = 0, border = "rounded" },
+          },
         },
+      },
     },
+  },
 }
