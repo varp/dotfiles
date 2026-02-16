a9b4991018219c523156d56fb9f0dbc933160fad
Vardan Pogosian <vardan.pogosyan@gmail.com>
2026-02-16 13:18:04 +0500

    neovim: properly configured DAP for Headless local debug

--- dotfiles/config/nvim/lua/plugins/dap-go.lua
+++ dotfiles/config/nvim/lua/plugins/dap-go.lua
@@ -3,14 +3,16 @@ return {
     "leoluz/nvim-dap-go",
     opts = function(_, opts)
       local dap = require("dap")
-      -- dap.setup(opts)
 
+      -- command to start headless: dlv debug --headless --listen=:38697 --api-version=2 --accept-multiclient < <(echo "some input to pass to process")
       local attachRemoteConfig = {
         type = "go",
-        name = "Attach remote",
+        name = "Connect to Headless (Local)",
         mode = "remote",
         request = "attach",
+        port = "38697",
       }
+
 
       if dap.configurations.go then
         table.insert(dap.configurations.go, attachRemoteConfig)
