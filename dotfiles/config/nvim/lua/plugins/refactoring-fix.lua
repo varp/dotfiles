7fd48efd43bbc65c6e75ce6a3f1eabf592c5e366
Vardan Pogosian <vardan.pogosyan@gmail.com>
2026-05-04 15:16:45 +0500

    nvim: added refactoring plugin fix

---
+++ dotfiles/config/nvim/lua/plugins/refactoring-fix.lua
@@ -0,0 +1,12 @@
+return {
+    {
+        "lewis6991/async.nvim",
+        lazy = true,
+    },
+    {
+        "ThePrimeagen/refactoring.nvim",
+        dependencies = {
+            "lewis6991/async.nvim",
+        },
+    },
+}
