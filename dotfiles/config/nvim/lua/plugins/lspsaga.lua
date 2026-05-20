e4a92aef60050ff9cf81de593b7c781b6be0bc73
Vardan Pogosian <vardan.pogosyan@gmail.com>
2026-05-20 12:36:52 +0500

    nvim: updated lazy + new packages + fixes

---
+++ dotfiles/config/nvim/lua/plugins/lspsaga.lua
@@ -0,0 +1,15 @@
+return {
+    {
+        'nvimdev/lspsaga.nvim',
+        config = function()
+            require('lspsaga').setup({
+                symbol_in_winbar = { enable = false },
+                lightbulb = { enable = false },
+            })
+        end,
+        dependencies = {
+            'nvim-treesitter/nvim-treesitter', -- optional
+            'nvim-tree/nvim-web-devicons',     -- optional
+        }
+    }
+}
